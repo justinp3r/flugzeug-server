5cf0035d2bad765cfbd7babd3b01ea0d0c164823
Justin Perrone <justinperrone1998@gmail.com>
2024-04-17 20:13:08 +0200

    Modulendung .js hinzugefügt

--- src/config/dev/dev.module.ts
+++ src/config/dev/dev.module.ts
@@ -16,7 +16,7 @@
  */
 import { DbPopulateService } from './db-populate.service.js';
 import { DevController } from './dev.controller.js';
-import { Flugzeug } from '../../flugzeug/entity/flugzeug.entity';
+import { Flugzeug } from '../../flugzeug/entity/flugzeug.entity.js';
 import { KeycloakModule } from '../../security/keycloak/keycloak.module.js';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
